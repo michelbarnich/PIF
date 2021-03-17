0d50371306494b824c27b0773ce55437595ad406
Michel Barnich <private@michelbarnich.com>
2021-03-17 12:42:53 +0100

    [/ajax/getPkantTelemetry.php] - preventing accessing data outside of the users company

--- ajax/getPlantTelemetry.php
+++ ajax/getPlantTelemetry.php
@@ -9,62 +9,75 @@
 
     if ($response["userData"]["role"] >= 1 ) {
 
-        $response = [];
+        $sql = "SELECT fiFirma FROM tblsensorstation WHERE idIdentifikationsNummer = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $_GET["sensorID"]);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $response["SQLError"] = $stmt->error;
 
-        if(!isset($_GET["dateTo"]) && !isset($_GET["dateFrom"])) {
+        $row = $result->fetch_assoc();
 
-            $sql = "SELECT * FROM tblsensordaten WHERE fiSensorStation = ? ORDER BY dtTimestamp DESC LIMIT 1;";
-            $stmt = $conn->prepare($sql);
-            $stmt->bind_param("s", $_GET["sensorID"]);
-            $stmt->execute();
-            $result = $stmt->get_result();
-            $response["SQLError"] = $stmt->error;
+        if($row["fiFirma"] == $response["userData"]["companyId"]) {
 
-            for ($i = 0; $i < $result->num_rows; $i++) {
-                $row = $result->fetch_assoc();
+            $response = [];
 
-                $response["plantTelemetry"] = [
-                    "humidity" => replaceSpecialChars($row["dtLuftfeuchtigkeit"]),
-                    "temperature" => replaceSpecialChars($row["dtTemperatur"]),
-                    "light" => replaceSpecialChars($row["dtHelligkeit"]),
-                    "moisture" => replaceSpecialChars($row["dtBodenfeuchtigkeit"])
-                ];
-            }
+            if(!isset($_GET["dateTo"]) && !isset($_GET["dateFrom"])) {
 
-        } else {
-
-            if($_GET["intervalRate"] == "h") {
-                $interval = 3600;
-            } elseif($_GET["intervalRate"] == "d") {
-                $interval = 86400 ;
-            } elseif($_GET["intervalRate"] == "w") {
-                $interval = 604800 ;
-            } elseif($_GET["intervalRate"] == "m") {
-                $interval = 2592000  ;
-            } elseif($_GET["intervalRate"] == "y") {
-                $interval = 31536000 ;
-            }
-
-            $iterations = ceil(($_GET["dateTo"] - $_GET["dateFrom"]) / $interval);
-            $dataArray = [];
-
-            for ($i = 0; $i < $iterations; $i++) {
-
-                //echo ($_GET["dateFrom"] + ($i * $interval)) . ", " . ($_GET["dateFrom"] + (($i + 1) * $interval)) . "<br>";
-
-                $sql = "SELECT AVG(dtTemperatur), AVG(dtLuftfeuchtigkeit), AVG(dtBodenfeuchtigkeit), AVG(dtHelligkeit) FROM tblsensordaten WHERE idSensorDatenNummer IN (SELECT idSensorDatenNummer  FROM tblsensordaten WHERE dtTimestamp > FROM_UNIXTIME(" . ($_GET["dateFrom"] + ($i * $interval)) . ") AND dtTimestamp < FROM_UNIXTIME(" . ($_GET["dateFrom"] + (($i + 1) * $interval)) . ") AND fiSensorStation = ?)";
+                $sql = "SELECT * FROM tblsensordaten WHERE fiSensorStation = ? ORDER BY dtTimestamp DESC LIMIT 1;";
                 $stmt = $conn->prepare($sql);
                 $stmt->bind_param("s", $_GET["sensorID"]);
                 $stmt->execute();
                 $result = $stmt->get_result();
                 $response["SQLError"] = $stmt->error;
 
-                $row = $result->fetch_assoc();
-                array_push($dataArray, [ replaceSpecialChars($row["AVG(dtLuftfeuchtigkeit)"]), replaceSpecialChars($row["AVG(dtTemperatur)"]), replaceSpecialChars($row["AVG(dtHelligkeit)"]), replaceSpecialChars($row["AVG(dtBodenfeuchtigkeit)"]), ($_GET["dateFrom"] + ($i * $interval))]);
+                for ($i = 0; $i < $result->num_rows; $i++) {
+                    $row = $result->fetch_assoc();
+
+                    $response["plantTelemetry"] = [
+                        "humidity" => replaceSpecialChars($row["dtLuftfeuchtigkeit"]),
+                        "temperature" => replaceSpecialChars($row["dtTemperatur"]),
+                        "light" => replaceSpecialChars($row["dtHelligkeit"]),
+                        "moisture" => replaceSpecialChars($row["dtBodenfeuchtigkeit"])
+                    ];
+                }
+
+            } else {
+
+                if($_GET["intervalRate"] == "h") {
+                    $interval = 3600;
+                } elseif($_GET["intervalRate"] == "d") {
+                    $interval = 86400 ;
+                } elseif($_GET["intervalRate"] == "w") {
+                    $interval = 604800 ;
+                } elseif($_GET["intervalRate"] == "m") {
+                    $interval = 2592000  ;
+                } elseif($_GET["intervalRate"] == "y") {
+                    $interval = 31536000 ;
+                }
+
+                $iterations = ceil(($_GET["dateTo"] - $_GET["dateFrom"]) / $interval);
+                $dataArray = [];
+
+                for ($i = 0; $i < $iterations; $i++) {
+
+                    //echo ($_GET["dateFrom"] + ($i * $interval)) . ", " . ($_GET["dateFrom"] + (($i + 1) * $interval)) . "<br>";
+
+                    $sql = "SELECT AVG(dtTemperatur), AVG(dtLuftfeuchtigkeit), AVG(dtBodenfeuchtigkeit), AVG(dtHelligkeit) FROM tblsensordaten WHERE idSensorDatenNummer IN (SELECT idSensorDatenNummer  FROM tblsensordaten WHERE dtTimestamp > FROM_UNIXTIME(" . ($_GET["dateFrom"] + ($i * $interval)) . ") AND dtTimestamp < FROM_UNIXTIME(" . ($_GET["dateFrom"] + (($i + 1) * $interval)) . ") AND fiSensorStation = ?)";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->bind_param("s", $_GET["sensorID"]);
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+                    $response["SQLError"] = $stmt->error;
+
+                    $row = $result->fetch_assoc();
+                    array_push($dataArray, [ replaceSpecialChars($row["AVG(dtLuftfeuchtigkeit)"]), replaceSpecialChars($row["AVG(dtTemperatur)"]), replaceSpecialChars($row["AVG(dtHelligkeit)"]), replaceSpecialChars($row["AVG(dtBodenfeuchtigkeit)"]), ($_GET["dateFrom"] + ($i * $interval))]);
+
+                }
+
+                array_push($response, $dataArray);
 
             }
-
-            array_push($response, $dataArray);
 
         }
 
