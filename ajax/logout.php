4ae5d2af50d9c4412d64d5c2ae459c2dd69b8f40
Michel Barnich <private@michelbarnich.com>
2021-03-24 16:03:21 +0100

    [/ajax/logout.php] - properly deletes session now

--- ajax/logout.php
+++ ajax/logout.php
@@ -1,6 +1,10 @@
 <?php
     include_once "../scripts/setUpSession.php";
+
+    setcookie (session_id(), "", time() - 3600);
     session_destroy();
+    session_write_close();
+    
     $response["logoutSuccess"] = true;
 
     echo json_encode($response);
