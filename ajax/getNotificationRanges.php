6c17074187a3906b7185869d82249898801b861e
Michel <private@michelbarnich.com>
2021-04-03 13:14:26 +0200

    show Info now displays the current thresholds for message trigegrs

---
+++ ajax/getNotificationRanges.php
@@ -0,0 +1,38 @@
+<?php
+    $silent = true; // if $silent is set to true, getUserData.php wont echo
+
+    // Connecting to Database
+    include_once "../scripts/connectToDatabase.php";
+    include_once "../scripts/setUpSession.php";
+    include_once "../ajax/getUserData.php";
+    include_once "../scripts/parseURLForQueries.php";
+
+    if ($response["userData"]["role"] >= 1 ) {
+
+        if($_GET["value"] == "moisture") {
+            $fields = "dtMinimumBodenfeuchtigkeit AS min, dtMaximumBodenfeuchtigkeit AS max";
+        } elseif($_GET["value"] == "humidity") {
+            $fields = "dtMinimumLuftfeuchtigkeit AS min, dtMaximumLuftfeuchtigkeit AS max";
+        } elseif($_GET["value"] == "light") {
+            $fields = "dtMinimumHelligkeit AS min, dtMinimumHelligkeit AS max";
+        } elseif($_GET["value"] == "temperature") {
+            $fields = "dtMinimumTemperatur AS min, dtMaximumTemperatur AS max";
+        }
+
+        $sql = "SELECT " . $fields . " FROM tblbenachrichtigung WHERE fiSensor = ? AND fiErstelltVon = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ss", $_GET["sensorID"], $_SESSION["id"]);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $response["SQLError"] = $stmt->error;
+
+        $row = $result->fetch_assoc();
+
+        $response["values"] = [
+            "max" => $row["max"],
+            "min" => $row["min"]
+        ];
+
+    }
+
+    echo json_encode($response);
